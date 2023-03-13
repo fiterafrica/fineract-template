1e4e9d7157755f83e495c50069579f698a7679f4
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2023-03-13 12:12:35 +0300

    OXY-83 whitelist nextWithdrawalDate param

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/data/SavingsAccountConstant.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/data/SavingsAccountConstant.java
@@ -59,7 +59,7 @@ public class SavingsAccountConstant extends SavingsApiConstants {
             Arrays.asList(idParamName, accountNoParamName));
 
     protected static final Set<String> SAVINGS_ACCOUNT_ACTIVATION_REQUEST_DATA_PARAMETERS = new HashSet<>(
-            Arrays.asList(localeParamName, dateFormatParamName, activatedOnDateParamName));
+            Arrays.asList(localeParamName, dateFormatParamName, activatedOnDateParamName,nextWithdrawalDate));
 
     protected static final Set<String> SAVINGS_ACCOUNT_CLOSE_REQUEST_DATA_PARAMETERS = new HashSet<>(
             Arrays.asList(localeParamName, dateFormatParamName, closedOnDateParamName, noteParamName, paymentTypeIdParamName,
