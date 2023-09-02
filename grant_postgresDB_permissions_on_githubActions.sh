142b1a141e5a0e9a8fde29a8d8618beceab8be65
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2023-09-02 13:53:53 +0300

    INKO-101 add support to create group with representative and get them back

--- grant_postgresDB_permissions_on_githubActions.sh
+++ grant_postgresDB_permissions_on_githubActions.sh
@@ -3,12 +3,10 @@
 # Wait for PostgreSQL to be ready (you can adjust the timeout as needed)
 echo "Waiting for PostgreSQL to be ready..."
 until pg_isready -h localhost -U postgres -q; do
-  sleep 1
+  sleep 5
 done
 
 # Grant permissions on the public schema
 echo "Granting permissions on the public schema..."
 psql -h localhost -U postgres -d fineract_default -c "GRANT ALL PRIVILEGES ON SCHEMA public TO fineract_default;"
-
-# Run the original Docker entrypoint command
-exec /usr/local/bin/docker-entrypoint.sh "$@"
+psql -h localhost -U postgres -d fineract_default -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO fineract_default;"
