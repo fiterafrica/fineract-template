d0e0d3af8408a45f611e70f2c6971d25e50ba5c7
Frank Nkuyahaga <hagafrank@gmail.com>
2023-11-22 10:14:35 +0300

    CI18-379: Fix posting of interest and WHT on premature closure

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/FixedDepositAccount.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/FixedDepositAccount.java
@@ -590,7 +590,7 @@ public class FixedDepositAccount extends SavingsAccount {
             if (postingTransaction == null) {
                 final SavingsAccountTransaction newPostingTransaction = SavingsAccountTransaction.interestPosting(this, office(),
                         interestPostingTransactionDate, interestEarnedToBePostedForPeriod, interestPostingPeriod.isUserPosting());
-                this.transactions.add(newPostingTransaction);
+                this.addNewTransaction(newPostingTransaction);
                 recalucateDailyBalanceDetails = true;
             } else {
                 final boolean correctionRequired = postingTransaction.hasNotAmount(interestEarnedToBePostedForPeriod);
@@ -598,7 +598,7 @@ public class FixedDepositAccount extends SavingsAccount {
                     postingTransaction.reverse();
                     final SavingsAccountTransaction newPostingTransaction = SavingsAccountTransaction.interestPosting(this, office(),
                             interestPostingTransactionDate, interestEarnedToBePostedForPeriod, interestPostingPeriod.isUserPosting());
-                    this.transactions.add(newPostingTransaction);
+                    this.addNewTransaction(newPostingTransaction);
                     recalucateDailyBalanceDetails = true;
                 }
             }
@@ -633,7 +633,7 @@ public class FixedDepositAccount extends SavingsAccount {
             final boolean postInterestAsOn = false;
             final SavingsAccountTransaction newPostingTransaction = SavingsAccountTransaction.interestPosting(this, office(),
                     accountCloseDate, remainigInterestToBePosted, postInterestAsOn);
-            this.transactions.add(newPostingTransaction);
+            this.addNewTransaction(newPostingTransaction);
             recalculateDailyBalance = true;
         }
 
