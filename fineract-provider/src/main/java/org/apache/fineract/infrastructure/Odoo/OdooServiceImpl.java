f47b2e87da02a0ef6172f75c028ad06206626011
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2024-02-20 11:40:19 +0300

    INKO-374 Refactor Odoo Integration code to accomodate changes for data migration

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/Odoo/OdooServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/Odoo/OdooServiceImpl.java
@@ -18,6 +18,7 @@
  */
 package org.apache.fineract.infrastructure.Odoo;
 
+import com.google.common.base.Splitter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -26,6 +27,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.fineract.accounting.journalentry.domain.JournalEntry;
 import org.apache.fineract.accounting.journalentry.domain.JournalEntryRepository;
 import org.apache.fineract.infrastructure.Odoo.exception.OdooFailedException;
@@ -78,11 +81,11 @@ public class OdooServiceImpl implements OdooService {
     @Override
     public Integer loginToOddo() {
         try {
-            final XmlRpcClientConfigImpl common_config = new XmlRpcClientConfigImpl();
+            final XmlRpcClientConfigImpl commonConfig = new XmlRpcClientConfigImpl();
             final XmlRpcClient client = new XmlRpcClient();
-            common_config.setServerURL(new URL(String.format("%s/xmlrpc/2/common", url)));
+            commonConfig.setServerURL(new URL(String.format("%s/xmlrpc/2/common", url)));
 
-            Object uid = (Object) client.execute(common_config, "authenticate",
+            Object uid = (Object) client.execute(commonConfig, "authenticate",
                     Arrays.asList(odooDB, username, password, Collections.emptyMap()));
             if (!uid.equals(false)) {
                 LOG.info("Odoo Authentication successful uid" + uid);
@@ -92,7 +95,7 @@ public class OdooServiceImpl implements OdooService {
                 return 0;
             }
         } catch (Exception e) {
-            LOG.error("Odoo Authentication failure message", e);
+            LOG.error(e.getMessage());
         }
         return 0;
     }
@@ -353,7 +356,7 @@ public class OdooServiceImpl implements OdooService {
                     try {
                         journalEntryDebitCredit.add(entry);
 
-                        if (journalEntryDebitCredit.size() > 1 && journalEntryDebitCredit.size() <= 2) {
+                        if (journalEntryDebitCredit.size() == 2) {
                             Integer id = createJournalEntryToOddo(journalEntryDebitCredit);
                             if (id != null) {
                                 for (JournalEntry je : journalEntryDebitCredit) {
@@ -406,14 +409,14 @@ public class OdooServiceImpl implements OdooService {
     }
 
     private Integer getGlAccounts(JournalEntry entry, Integer uid, XmlRpcClient models) {
+        Integer id = null;
         try {
             if (uid > 0) {
                 final List glAccount = Arrays.asList((Object[]) models.execute("execute_kw",
                         Arrays.asList(odooDB, uid, password, "account.account", "search_read",
-                                Arrays.asList(Arrays.asList(Arrays.asList("code", "=", entry.getGlAccount().getGlCode()))),
+                                Arrays.asList(Arrays.asList(Arrays.asList("code", "=", extractGlCode(entry.getGlAccount().getGlCode())))),
                                 Map.of("fields", Arrays.asList("id"), "limit", 5))));
-                Integer id = null;
-                if (glAccount != null && glAccount.size() > 0) {
+                if (!CollectionUtils.isEmpty(glAccount)) {
                     HashMap account = (HashMap) glAccount.get(0);
                     id = (Integer) account.get("id");
                 }
@@ -423,7 +426,31 @@ public class OdooServiceImpl implements OdooService {
         } catch (XmlRpcException e) {
             throw new OdooFailedException(e);
         }
-        return null;
+        return id;
+    }
+
+    /*
+     * This will help extract the GL Code from the concatenated GL code with id posted to fineract during data
+     * migration. The Code on Odoo is not unique so we concatenate the GL code with the GL id {code_id} so we need to
+     * extract the GLCode from the code again if we want the integration with Odoo from Fineract work as expected. We
+     * accommodated the aspect of the GL code not concatenated with the id if this is created in fineract direct
+     */
+    private Integer extractGlCode(String glCode) {
+        if (glCode.contains("_")) {
+            List<String> parts = Splitter.on(Pattern.compile("-", Pattern.LITERAL)).splitToList(glCode);
+
+            try {
+                return Integer.parseInt(parts.get(0));
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        } else {
+            try {
+                return Integer.parseInt(glCode);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
     }
 
 }
