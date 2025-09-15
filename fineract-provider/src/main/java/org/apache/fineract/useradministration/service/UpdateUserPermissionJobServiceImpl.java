9c954970364e37f27a1e20681df51f65f855c6e4
hero78 <johnboscomwesigye@gmail.com>
2025-09-15 11:02:21 +0300

    FSO-272 Process Checker User

---
+++ fineract-provider/src/main/java/org/apache/fineract/useradministration/service/UpdateUserPermissionJobServiceImpl.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.useradministration.service;
+
+import org.apache.fineract.infrastructure.jobs.annotation.CronTarget;
+import org.apache.fineract.infrastructure.jobs.service.JobName;
+import org.apache.fineract.useradministration.domain.AppUser;
+import org.apache.fineract.useradministration.domain.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UpdateUserPermissionJobServiceImpl {
+
+    private final AppUserRepository appUserRepository;
+    private final UserPermissionService userPermissionService;
+
+    @Autowired
+    public UpdateUserPermissionJobServiceImpl(AppUserRepository appUserRepository, UserPermissionService userPermissionService) {
+        this.appUserRepository = appUserRepository;
+        this.userPermissionService = userPermissionService;
+    }
+
+    @CronTarget(jobName = JobName.PROCESS_MAKER_CHECKER_USERS)
+    public void updateUserPermissions() {
+        List<AppUser> users = appUserRepository.findAll();
+        for (AppUser user : users) {
+            boolean isChecker = userPermissionService.isChecker(user);
+            user.setChecker(isChecker);
+            appUserRepository.save(user);
+        }
+    }
+}
